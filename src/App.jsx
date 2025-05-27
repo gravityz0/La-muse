17614064384fffdd9c9a30f37352e5c026fe49a3
divin_d1 <gravityz0071@gmail.com>
2025-05-27 11:49:54 +0200

    My second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import Navbar from './Components/Navbar'
 import NewArrival from './Components/NewArrival'
 import Card from './Components/Card'
 import Testimonials from './Components/Testimonials'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
+// import Test from './Components/Test'
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar/> */}
-      {/* <NewArrival/> */}
+      <Navbar/>
+      <NewArrival/>
       <Card/>
       <Testimonials/>
+      <Contact/>
+      <Footer/>
+      {/* <Test/> */}
     </div>
   )
 }
